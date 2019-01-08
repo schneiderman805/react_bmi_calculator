01956974d2a6a481f1c72ffdfd0ef3d3fc8c9476
William Schneiderman <william.m.schneiderman@gmail.com>
2019-01-08 13:17:22 +0100

    adds input for weight and height

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <div>
+          <label>Weight(kg)</label>
+          <input name="weight" />
+        </div>
+
+        <div>
+          <label>Height(cm)</label>
+          <input name="height" />
+        </div>
       </div>
     );
   }
