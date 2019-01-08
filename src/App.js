750be48333f746d0f125ac881abbcc4459ffa463
William Schneiderman <william.m.schneiderman@gmail.com>
2019-01-08 13:24:54 +0100

    creates function to declare the state and the default value of our states

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      weight: '',
+      height: ''
+    }
+  }
   render() {
     return (
       <div className="App">
