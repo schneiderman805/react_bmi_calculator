4aeae15a3ea0ef8a33601d1e099ae9fa3f788d52
William Schneiderman <william.m.schneiderman@gmail.com>
2019-01-08 15:30:46 +0100

    fixes errors regarding imports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+import DisplayResult from './DisplayResult';
+
 
 class App extends Component {
   constructor(props) {
--- src/DisplayResult.js
+++ src/DisplayResult.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import {bmiCalculation} from './BMICalculator'
 
 class DisplayResult extends Component {
     calculate() {
         var weight= this.props.weight;
         var height= this.props.height;
+
+        return bmiCalculation(weight, height);
     }
 
   render() {
