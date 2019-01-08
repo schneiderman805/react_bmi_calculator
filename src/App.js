26a9bcd6e758103291bd41956ee828f665e56639
William Schneiderman <william.m.schneiderman@gmail.com>
2019-01-08 13:28:53 +0100

    adds function to display result

--- src/App.js
+++ src/App.js
@@ -21,6 +21,10 @@ class App extends Component {
           <label>Height(cm)</label>
           <input name="height" value={this.state.height} onChange={(e) => this.setState({ height: e.target.value })} />
         </div>
+        <DisplayResult
+          weight={this.state.weight}
+          height={this.state.height}
+        />
       </div>
     );
   }
