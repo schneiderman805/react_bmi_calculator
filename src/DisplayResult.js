f3e038c579f6b473e6906cb5e513c21119c3628a
William Schneiderman <william.m.schneiderman@gmail.com>
2019-01-08 13:32:18 +0100

    adds funtion to claculate weight and height

--- src/DisplayResult.js
+++ src/DisplayResult.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 
 class DisplayResult extends Component {
+    calculate() {
+        var weight= this.props.weight;
+        var height= this.props.height;
+    }
 
   render() {
     return (
-      <div id='response'>
-      </div>
+        <div id='response'>
+            {this.calculate()}
+        </div>
     )
   }
 }
