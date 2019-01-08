8c8debad0a5eacfae6bf972642d2da3c3da5de1d
William Schneiderman <william.m.schneiderman@gmail.com>
2019-01-08 13:21:29 +0100

    adds function to save input data

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ class App extends Component {
       <div className="App">
         <div>
           <label>Weight(kg)</label>
-          <input name="weight" />
+          <input name="weight" value={this.state.weight} onChange={(e) => this.setState({ weight: e.target.value })} />
         </div>
 
         <div>
           <label>Height(cm)</label>
-          <input name="height" />
+          <input name="height" value={this.state.height} onChange={(e) => this.setState({ height: e.target.value })} />
         </div>
       </div>
     );
